2bea2bf80f14068f681a55d8c4aea72b314e1d57
thiago.freitas <thiago.freitas@cepedi.org.br>
2022-05-05 20:20:03 -0300

    Auteração

--- AutoLuby-EM-ANDAMENTO/src/Controllers/account.controller.ts
+++
@@ -1,8 +0,0 @@
-import AccountRepository from "../Models/account/account.model";
-
-async function findAll(req, res) {
-  const clients = await AccountRepository.findAll();
-  res.json(clients);
-}
-
-export default { findAll };
